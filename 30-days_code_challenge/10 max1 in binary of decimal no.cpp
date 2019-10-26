1a71754fd022d1c0cc23f4af4b4024cc376c6184
Luvkumar8436 <luvkumar8436@gmail.com>
2019-10-26 13:20:26 +0530

    maximum no 0f 1 into binary form of decimal number

---
+++ 30-days_code_challenge/10 max1 in binary of decimal no.cpp
@@ -0,0 +1,87 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+
+
+int main()
+{
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE);}
+    scanf("%d",&n);
+   
+    int max1_curr=0,max1_prev=0,highest_max1=0;
+    scanf("%d",&n);
+
+    while(!(n==1))
+    {
+        if(n%2==1)
+        {
+            max1_curr=max1_curr+1;
+        }
+        else {
+            if(max1_prev>max1_curr)
+            {
+                highest_max1=max1_prev;
+            }
+            else {
+                highest_max1=max1_curr;
+                }
+        max1_prev=max1_curr;
+        max1_curr=0;    
+
+        }
+        n=(n-(n%2))/2;
+        
+    }
+   
+        if(highest_max1<max1_curr+1)
+        highest_max1=max1_curr+1;
+
+    
+    printf("%d",highest_max1);
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
