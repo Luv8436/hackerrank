f44ca4895d5c419ce164be3d347336f3bc332613
Luv Kumar <51437281+Luv8436@users.noreply.github.com>
2020-04-24 09:10:50 +0530

    Add files via upload
    Complete the getHeight or height function in the editor. It must return the height of a binary tree as an integer.
    
    getHeight or height has the following parameter(s):
    
    root: a reference to the root of a binary tree.

---
+++ Problem Solving/c/height of binary tree.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+struct node* insert( struct node* root, int data ) {
+		
+	if(root == NULL) {
+	
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+
+        node->data = data;
+
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+	  
+	} else {
+      
+		struct node* cur;
+		
+		if(data <= root->data) {
+            cur = insert(root->left, data);
+            root->left = cur;
+		} else {
+            cur = insert(root->right, data);
+            root->right = cur;
+		}
+	
+		return root;
+	}
+}
+int maximum(int a, int b);
+/* you only have to complete the function given below.  
+node is defined as  
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+*/
+int getHeight(struct node* root) {
+    // Write your code here
+    if(root==NULL)
+    {
+        return -1;
+    }
+    int height_left = getHeight(root->left);
+    int height_right = getHeight(root->right);
+    return 1+maximum(height_left , height_right);
+}
+int maximum(int a, int b)
+{
+    if(a>b)
+    return a;
+    else {
+    return b;
+    }
+}
+
